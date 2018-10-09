fa1fd1d7bd17fa70b1a9be3babf9134429565458
william1993jda <34724110+william1993jda@users.noreply.github.com>
2018-10-09 18:22:45 -0300

    Teste

--- js/meu.js
+++ js/meu.js
@@ -13,18 +13,8 @@ $(document).ready(function() {
                 email: true
             },
 
-            ddd: {
-                required: true,
-                number: true,
-                maxlength: 2,
-                minlength: 2
-            },
-
             Fone: {
                 required: true,
-                number: true,
-                maxlength: 12,
-                minlength: 8
             },
 
             Mensagem: {
@@ -32,4 +22,31 @@ $(document).ready(function() {
             }
         }
     });
+});
+
+$("#phone").bind('input propertychange',function() {
+
+    var texto = $(this).val();
+
+    texto = texto.replace(/[^\d]/g, '');
+
+    if (texto.length > 0)
+    {
+        texto = "(" + texto;
+
+        if (texto.length > 3)
+        {
+            texto = [texto.slice(0, 3), ") ", texto.slice(3)].join('');
+        }
+        if (texto.length > 12)
+        {
+            if (texto.length > 13)
+                texto = [texto.slice(0, 10), "-", texto.slice(10)].join('');
+            else
+                texto = [texto.slice(0, 9), "-", texto.slice(9)].join('');
+        }
+        if (texto.length > 15)
+            texto = texto.substr(0,15);
+    }
+    $(this).val(texto);
 });
